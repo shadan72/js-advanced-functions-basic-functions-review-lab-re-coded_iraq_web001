2446f1ef4597f16f5a996b5a2814ccedf7e38f2c
Shadan Abdulkarim <shadanabdulkarim94@gmail.com>
2020-06-07 21:49:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+  let msg=`This Saturday, I want to ${activity}!`;
+  return msg;
+}
+saturdayFun();
+saturdayFun("I want to bathe my dog");
+
+
+let mondayWork= function(activity="go to the office"){
+  let msg=`This Monday, I will ${activity}.`;
+  return msg;
+};
+
+function wrapAdjective(param1='*'){
+  return function(param2="special"){
+   
+   let msg=`You are ${param1}${param2}${param1}!` ;
+   return msg;
+  };
+}
+
+const Calculator={
+  add: (a,b)=>a + b,
+  subtract: (a,b)=>a - b,
+  multiply: (a,b)=>a * b,
+  divide: (a,b)=>a / b
+}
+
+function actionApplyer(intStart, ArrayOfFunc){
+  if(ArrayOfFunc=='')
+  return intStart;
+
+else {
+  ArrayOfFunc.forEach(innerFunc=>{
+    intStart=innerFunc(intStart)
+  })
+      return intStart
+}
+}
+
+
